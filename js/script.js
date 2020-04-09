32abcac5c37ef39fa9a527a6a2a354d4766154c8
joshuanwankwo <systemblakes@gmail.com>
2020-04-09 01:10:27 +0100

    made adjustment to some of the features

--- js/script.js
+++ js/script.js
@@ -9,23 +9,23 @@ for(i=0; i <= el.length; i++){
 
 console.log(document.querySelectorAll('.plartform'));
 
+
+var newLink = document.querySelectorAll("#mobileNavigation > a");
+for( i = 0; i <= newLink.length; i++){
+    newLink[i].style.display = 'none'
+}
+
 function showMenu(params) {
 
     document.getElementById("menu").style.display = "none";
 
-    document.getElementById("times").style.display = "block"
+    document.getElementById("times").style.display = "block";
 
-    document.getElementById("mobileNavigation").style.height = "100vh"
+    document.getElementById("mobileNavigation").style.height = "100vh";
 
     document.getElementById("mobileNavigation").style.display = "flex";
 
 }
-
-var newLink = document.querySelectorAll("#mobileNavigation > a");
-for( i = 0; i <= newLink.length; i++){
-    newLink[i].style.display = "none";
-}
-
 
 
 
